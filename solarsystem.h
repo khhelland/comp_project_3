b10f442fe55a6c415c116684ac75c369b77e32bb
Knut Halvor Helland <knut.helland@gmail.com>
2014-10-06 17:53:15 +0200

    begynte på solsystemklasse. La tanker om endringer i kommentarer der

---
+++ solarsystem.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <armadillo>
+#include <vector>
+#include "planet.h"
+#include "ODE.h"
+
+using namespace arma;
+//Class for modeling
+//solarsystem
+
+class solarsystem
+{
+ public:
+  solarsystem(){}
+  vector<planet>  planets;
+  vec init;
+  double T;
+  char* method;
+  int rk4 = 1;
+  int verlet = 0;
+  //maybe change ODE so that U is here
+  //also let user decide if U shoulf be 
+  //held in memory, or let it not be with no option
+  mat states;
+  vec state;
+  
+  solarsystem(int);//argument is number of "planets"
+  solarsystem(vector<planet>);//
+  vec derivatives(vec,double);
+  vec doublederivatives(vec,double);
+  void add_planet(planet);
+  void use_rk4(int); //kanskje kan disse bli til en?
+  void use_verlet(int);
+  void reset();
+  
+};
