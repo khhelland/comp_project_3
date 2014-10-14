62c2a1c9f7d6b5b8b458642f51b213dc49146df8
Knut Halvor Helland <knut.helland@gmail.com>
2014-10-14 15:33:21 +0200

    La til valg om hvorvidt solen står fast i midten eller ikke. Endret dds slik at verlet også skriver ut hastigheter, men de bør bli null. Nå ser både verlet og rk4 ut til å funke.

--- main.cpp
+++ main.cpp
@@ -11,21 +11,28 @@ int main()
 {
   solarsystem sys;
   
-  planet sun(1,0,0,0,0,(2*_pi*5.2*pow(10,-3)/12) - 2*_pi*3*pow(10,-6),0);
-  sys.add_planet(sun);
+  // planet sun(1,0,0,0,0,(2*_pi*5.2*pow(10,-3)/12) - 2*_pi*3*pow(10,-6),0);
+  // sys.add_planet(sun);
 
   planet earth(3*pow(10,-6),1,0,0,0,2*_pi,0);
   sys.add_planet(earth);
  
   planet jupiter(pow(10,-3),-5.2,0,0,0,-2*_pi*5.2/12,0);
   sys.add_planet(jupiter);
+  
+  // planet comet(pow(10,-7),2,2,0,0,0,0);
+  // sys.add_planet(comet);
 
   sys.h = 0.01;
-  sys.T = 50;
+  sys.T = 36;
+  sys.sun_in_origin = 1;
+  sys.use_verlet = 1;
   sys.initialize();
 
-  sys.rk4("system.dat");
-    
+  sys.simulate("system.dat");
+  
+  
+  
   return 0;
 }
 
--- solarsystem.h
+++ solarsystem.h
@@ -8,25 +8,28 @@
 
 class solarsystem
 {
- public:
-  //tror systemet kan bestaa av mange planeter
-  //og en ODE
-  solarsystem(): t(0) {}
+private:
+  void update_planets();
+
+public:
+  solarsystem(): t(0), use_verlet(0), sun_in_origin(0) {}
   std::vector<planet>  planets;
   arma::vec init;
   arma::vec state;
   double t;
   double T ;
   double h;
+  
+  bool use_verlet;
  
+  bool sun_in_origin;
+  
   void rk4(const char*);
   void verlet(const char*);
   arma::vec derivatives(arma::vec);
   arma::vec doublederivatives(arma::vec);
   void add_planet(planet);
   void initialize();
-  void update_planets();
   void reset();
-  
-  
+  void simulate(const char*);
 };
